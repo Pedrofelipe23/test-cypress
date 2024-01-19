985ba4ffaacceafce7738abf6488dad7a7904fc6
Pedro Pereira <pedrofelipee23@gmail.com>
2024-01-19 18:13:46 -0300

    new test page alterarSenha

---
+++ cypress/e2e/bancarTest/alterarSenha.cy.js
@@ -0,0 +1,12 @@
+describe('BanCar - alterarSenha',() => {
+
+    beforeEach(() =>{
+        cy.visit('/pages/alterarSenha')
+        cy.login('matheusmws31@gmail.com', '12345678Ti@')
+    })
+
+    it('page to change your password', () => {
+        
+    });
+
+})
